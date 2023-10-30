e5e64a8e9323c85198c9d18b6debe0588f177232
ChetanKumar-Sura <81322260+chetankumar-sura@users.noreply.github.com>
2023-10-30 12:30:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ tip_as_percent = tip / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people
-final_amount = round(bill_per_person, 2)
+final_amount = round(bill_per_person, 2) #  How to round to 2 decimal places?
 
-
-#  How to round to 2 decimal places?
 
 print(f"Each person should pay: ${final_amount}")
